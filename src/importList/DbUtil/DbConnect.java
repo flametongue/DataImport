3511fb07e0eabe951ab33a3ad51172d5d7e7aa39
feazen <743860161@qq.com>
2014-11-24 17:29:12 +0800

    11-24初始化OK，可以读到Excel里内容。构建Inof，待优化结构

--- src/importList/DbUtil/DbConnect.java
+++ src/importList/DbUtil/DbConnect.java
@@ -1,30 +1,35 @@
 package importList.DbUtil;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class DbConnect {
-	public void Execute(String sql) throws ClassNotFoundException, SQLException {
+	public void Execute(String sql) throws ClassNotFoundException, SQLException, ParserConfigurationException, SAXException, IOException {
 		Connection dbConn = createConnection();
 		System.out.println("Connection Successful!"); // 如果连接成功 控制台输出Connection
-														// Successful!
-		// DatabaseMetaData rs = dbConn.getMetaData();
 		Statement st = dbConn.createStatement();
 		st.execute(sql);
-		// ResultSet rs = st.executeQuery(sql);
-		// while(rs.next()){
-		// String idcard = rs.getString("idcard");
-		// System.out.println(idcard);
-		// }
 		st.close();
 		dbConn.close();
 	}
 
 	public ResultSet ExecuteSql(String sql) throws ClassNotFoundException,
-			SQLException {
+			SQLException, ParserConfigurationException, SAXException, IOException {
 		Connection dbConn = createConnection();
 		System.out.println("Connection Successful!"); // 如果连接成功 控制台输出Connection
 														// Successful!
@@ -32,17 +37,29 @@ public class DbConnect {
 		Statement st = dbConn.createStatement();
 		st.execute(sql);
 		ResultSet rs = st.executeQuery(sql);
-		st.close();
-		dbConn.close();
 		return rs;
 	}
 
 	public Connection createConnection() throws ClassNotFoundException,
-			SQLException {
-		String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver"; // 加载JDBC驱动
-		String dbURL = "jdbc:sqlserver://114.80.116.56; DatabaseName=hrchannel_data"; // 连接服务器和数据库test
-		String userName = "brqy"; // 默认用户名
-		String userPwd = "5thnji9876"; // 密码
+			SQLException, ParserConfigurationException, SAXException, IOException {
+		String driverName = null;; // JDBC驱动 名称
+		String dbURL = null;;// 连接服务器和数据库test
+		String userName = null; // 默认用户名
+		String userPwd = null;// 密码
+		DocumentBuilderFactory dbf = DocumentBuilderFactory .newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		File file = new File("DBConfig.xml");
+		Document doc = db.parse(file); doc.getDocumentElement().getElementsByTagName("driverName");doc.getDocumentElement().getElementsByTagName("root");
+		doc.getDocumentElement().getChildNodes().item(0);
+		NodeList root = doc.getElementsByTagName("myRoot"); doc.getElementsByTagName("driverName");
+		NodeList list = doc.getElementsByTagName("myRoot");
+		for (int i = 0; i < list.getLength(); i++) {
+			Element e = (Element) list.item(i);
+			driverName = e.getElementsByTagName("driverName").item(0).getFirstChild().getNodeValue();  
+			dbURL = e.getElementsByTagName("dbURL").item(0).getFirstChild().getNodeValue();
+			userName = e.getElementsByTagName("userName").item(0).getFirstChild().getNodeValue();
+			userPwd = e.getElementsByTagName("userPwd").item(0).getFirstChild().getNodeValue();
+		}
 		Connection dbConn;
 		Class.forName(driverName);
 		dbConn = DriverManager.getConnection(dbURL, userName, userPwd);
--- src/importList/Init.java
+++ src/importList/Init.java
@@ -1,30 +1,17 @@
 package importList;
+
+import importList.DbUtil.DbConnect;
+import importList.tableinfo.FundRateInfo;
+
 import java.io.File;
 import java.io.IOException;
-import java.rmi.server.Operation;
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-
-
 import java.sql.ResultSet;
-import java.sql.Statement;
-
-
-
-
-
-
-
-
-
+import java.sql.SQLException;
 
 import javax.swing.JFileChooser;
-/**
- * 描述：中企数据表格导入
- * @author feazen
- */
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import jxl.Cell;
 import jxl.Sheet;
@@ -36,28 +23,46 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 
 import javax.swing.JButton;
-import javax.swing.AbstractAction;
 
 import java.awt.event.ActionEvent;
-
-import javax.swing.Action;
-
 import java.awt.event.ActionListener;
 
-import javax.swing.JTextField;
 import javax.swing.JLabel;
 
+/**
+ * 描述：中企数据表格导入
+ * 
+ * @author feazen
+ */
 public class Init {
 	static JFrame f;
-	public static void main(String[] args) throws BiffException, IOException {
-		
+
+	public static void main(String[] args) throws BiffException, IOException,
+			ClassNotFoundException, SQLException {
+
 		initUI();
-//		connectDataBase();
-//		chooseFile();
+		// int s = JOptionPane.showConfirmDialog(null, "是否继续", "wtf",
+		// JOptionPane.YES_NO_OPTION);
+
+		// DbConnect dc = new DbConnect();
+		// try {
+		// Connection conn = dc.createConnection();
+		// ResultSet rs =
+		// dc.ExecuteSql("SELECT phone,mail FROM .[t_employee] where personName = '测试——2'");
+		// while (rs.next()) {
+		// String phone = rs.getString("phone");
+		// String mail = rs.getString("mail");
+		// System.out.println("查询成功：电话"+phone+"--邮箱"+mail);
+		// }
+		// conn.close();
+		// } catch (Exception e) {
+		// e.printStackTrace();
+		// }
 	}
-	
+
 	/**
 	 * 描述：选择导入的Excel文件
+	 * 
 	 * @throws BiffException
 	 * @throws IOException
 	 */
@@ -66,68 +71,70 @@ public class Init {
 		int results = jfChooser.showOpenDialog(f);
 		if (results == JFileChooser.APPROVE_OPTION) {
 			File file = jfChooser.getSelectedFile();// 根据文件名创建一个文件对象
-//			file.setReadOnly();
 			Workbook wb = Workbook.getWorkbook(file);// 从文件流中取得Excel工作区对象
 			Sheet sheet = wb.getSheet(0);
-			
-			for (int i = 1; i < sheet.getRows(); i++) {
+
+			for (int i = 6; i < sheet.getRows(); i++) {
+				FundRateInfo fundRateInfo = new FundRateInfo();//fundRateInfo.getClass().getDeclaredFields()[1].set(obj, value);
 				for (int j = 0; j < sheet.getColumns(); j++) {
 					Cell cell = sheet.getCell(j, i);
 					String sTemp = cell.getContents();
+					if (sTemp != null)
 						sTemp = sTemp.trim();
-						System.out.println(sTemp);
+//					fundRateInfo.set(key, value);
 				}
-				wb.close();
-//				file.
 			}
+			wb.close();
 		}
 	}
-
 
 	/**
 	 * 描述：初始化界面
 	 */
 	private static void initUI() {
-//		JFrame f = new JFrame("init");
-//		f.setVisible(true);
 		f = new JFrame();
 		f.setSize(404, 400);
-		
+
 		JPanel panel = new JPanel();
 		f.getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
-		
-		JButton btnfoundrateimport = new JButton("\u5BFC\u5165\u5E74\u5EA6\u5404\u5730\u793E\u4FDD\u516C\u79EF\u91D1\u57FA\u6570\u6BD4\u4F8B");
+
+		JButton btnfoundrateimport = new JButton(
+				"\u5BFC\u5165\u5E74\u5EA6\u5404\u5730\u793E\u4FDD\u516C\u79EF\u91D1\u57FA\u6570\u6BD4\u4F8B");
 		btnfoundrateimport.addActionListener(new ActionListener() {
-			//年度各地社保公积金基数比例表导入
+			// 年度各地社保公积金基数比例表导入
 			public void actionPerformed(ActionEvent e) {
 				try {
 					chooseFile();
 				} catch (BiffException | IOException e1) {
-					e1.printStackTrace();
+					JOptionPane.showConfirmDialog(f, e1.getMessage());
 				}
 			}
 		});
 		btnfoundrateimport.setBounds(28, 47, 253, 23);
 		panel.add(btnfoundrateimport);
-		
+
 		JButton btnNewButton_1 = new JButton("New button");
 		btnNewButton_1.setBounds(24, 80, 93, 23);
 		panel.add(btnNewButton_1);
-		
+
 		JButton btnNewButton_2 = new JButton("New button");
 		btnNewButton_2.setBounds(24, 128, 93, 23);
 		panel.add(btnNewButton_2);
-		
+
 		JButton btnNewButton_3 = new JButton("New button");
 		btnNewButton_3.setBounds(24, 171, 93, 23);
 		panel.add(btnNewButton_3);
-		
+
 		JLabel lbtitle = new JLabel("New label");
 		lbtitle.setBounds(28, 22, 287, 15);
 		lbtitle.setText("中企人力外部信息Excel导入--仅限内部使用");
 		panel.add(lbtitle);
 		f.setVisible(true);
 		f.setDefaultCloseOperation(3);
+	}
+
+	private void showInfo() {
+
 	}
 }
